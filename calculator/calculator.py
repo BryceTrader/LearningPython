60f30d8c922d469de4a78fbc4deed33b56c585cc
VoicelessSpirit <>
2022-04-20 07:00:05 +0000

    calculator working with spaced inputs

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -17,18 +17,18 @@ def math(a, operator, b):
 	switch = {
 		'+': plus,
 		'-': minus,
-		'x': times,
+		'*': times,
 		'/': divide
 	}
 
 	return switch[operator](a, b)
 
-def order_of_operations(ui):
+def order_of_operations(ui, opts):
 	for i, char in enumerate(ui):
-		if char == '/' or char == 'x':
-			a = int(ui[i-1])
+		if char == opts[0] or char == opts[1]:
+			a = int(ui[i-1]) if isinstance(ui[i-1], str) else ui[i-1]
 			opt = ui[i]
-			b = int(ui[i+1])
+			b = int(ui[i+1]) if isinstance(ui[i+1], str) else ui[i+1]
 			
 			lower = ui[:i-1] + [None, None]
 			middle = [math(a, opt, b)]
@@ -36,23 +36,22 @@ def order_of_operations(ui):
 			ui = lower + middle + upper
 			i -= 2
 	# removing None indexes
-	ui = [x for x in ui if x]
-	
-	for i, char in enumerate(ui):
-		if char == '+' or char == '-':
-			a = int(ui[i-1])
-			opt = ui[i]
-			b = int(ui[i+1])
-			
-			lower = ui[:i-1] + [None, None]
-			middle = [math(a, opt, b)]
-			upper = ui[i+2:]
-			ui = lower + middle + upper
-			i -= 2
-	# removing None indexes
-	ui = [x for x in ui if x]
+	return [x for x in ui if x]
+
+def use_input(ui):
+	opts = [['*', '/'], ['+', '-']]
+	for opt in opts:
+		ui = order_of_operations(ui, opt)
+
 	return ui[0]
 
+def filter_input(ui):
+	for i, x in enumerate(ui):
+		if x == __file__[2:]:
+			ui[i] = '*'
+	print(ui)
+	return use_input(ui)
+
 user_input = sys.argv[1:]
-result = order_of_operations(user_input)
+result = filter_input(user_input)
 print(result)
