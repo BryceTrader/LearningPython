7193a45ac883b108e069ee8a59b028e5b4af35c3
VoicelessSpirit <>
2022-04-20 06:43:39 +0000

    had to change * to x, OrderOfOps working

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 
 def plus(a, b):
 	return a + b
@@ -12,13 +13,46 @@ def times(a, b):
 def divide(a, b):
 	return a / b
 
-def math(a, b, operator):
+def math(a, operator, b):
 	switch = {
 		'+': plus,
 		'-': minus,
-		'*': times,
+		'x': times,
 		'/': divide
 	}
 
 	return switch[operator](a, b)
 
+def order_of_operations(ui):
+	for i, char in enumerate(ui):
+		if char == '/' or char == 'x':
+			a = int(ui[i-1])
+			opt = ui[i]
+			b = int(ui[i+1])
+			
+			lower = ui[:i-1] + [None, None]
+			middle = [math(a, opt, b)]
+			upper = ui[i+2:]
+			ui = lower + middle + upper
+			i -= 2
+	# removing None indexes
+	ui = [x for x in ui if x]
+	
+	for i, char in enumerate(ui):
+		if char == '+' or char == '-':
+			a = int(ui[i-1])
+			opt = ui[i]
+			b = int(ui[i+1])
+			
+			lower = ui[:i-1] + [None, None]
+			middle = [math(a, opt, b)]
+			upper = ui[i+2:]
+			ui = lower + middle + upper
+			i -= 2
+	# removing None indexes
+	ui = [x for x in ui if x]
+	return ui[0]
+
+user_input = sys.argv[1:]
+result = order_of_operations(user_input)
+print(result)
