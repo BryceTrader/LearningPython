9751525b797bbd40c9796d6d5b3228fb8b6bc676
VoicelessSpirit <>
2022-04-20 01:10:39 +0000

    Fizzbuzz done

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,3 +1,20 @@
-# divisible by 3 fizz
-# divisible by 5 buzz
-# divisible by both 3 and 5, fizzbuzz
+#!/usr/bin/env python
+import sys
+
+def fizzbuzz(y):
+	for x in range(1, y+1):
+		if (x % 3 == 0) and (x % 5 == 0):
+			print('Fizzbuzz')
+		elif x % 3 == 0:
+			print('Fizz')
+		elif x % 5 == 0:
+			print('Buzz')
+		else:
+			print(x)
+
+upto = sys.argv[1]
+if upto.isdecimal():
+	upto = int(upto)
+	fizzbuzz(upto)
+else:
+	print('Invalid input.')
